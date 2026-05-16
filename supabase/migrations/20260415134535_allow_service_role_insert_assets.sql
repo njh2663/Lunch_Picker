fedd5470d670174d1e0f2a6b07ff09f86d54ff14
JaeHyeok <103977462+njh2663@users.noreply.github.com>
2026-05-16 11:00:05 +0900

    Delete supabase directory

--- supabase/migrations/20260415134535_allow_service_role_insert_assets.sql
+++
@@ -1,9 +0,0 @@
-
-/*
-  # Allow service role to insert into assets bucket
-*/
-
-CREATE POLICY "Service role insert for assets"
-  ON storage.objects FOR INSERT
-  TO service_role
-  WITH CHECK (bucket_id = 'assets');
